cae1a4067e59df4bf2bcf0627e481303de6f0a2e
Garrett Rowell <garrett@puppet.com>
2023-06-20 12:23:29 -0400

    add a fail flag

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  if $facts['fail_catalog'] {
+    fail('i was told to fail...')
+  }
 }
 
 #node 'pe-nixagent-0.garrett.rowell' {
