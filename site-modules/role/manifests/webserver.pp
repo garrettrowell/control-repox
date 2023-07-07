8d7cd88783c25adc24996f57d4ede394ad9b8554
Garrett Rowell <garrett@puppet.com>
2023-07-07 12:32:22 -0400

    notify

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -1,7 +1,6 @@
 # This role would be made of all the profiles that need to be included to make a webserver work
 # All roles should include the base profile
 class role::webserver {
-
+  notify { 'hello from role::webserver': }
   include profile::base
-
 }
