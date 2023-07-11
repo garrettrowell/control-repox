cc75fca91d9bbd96599e6b9f4fafbbd731b30341
Garrett Rowell <garrett@puppet.com>
2023-07-11 16:01:06 -0400

    zip

--- site-modules/profile/manifests/base/windows.pp
+++ site-modules/profile/manifests/base/windows.pp
@@ -1,4 +1,8 @@
 class profile::base::windows {
   echo { 'hello from profile::base::windows': }
   include chocolatey
+  package { '7zip':
+    ensure   => '23.1.0',
+    provider =>  'chocolatey'
+  }
 }
