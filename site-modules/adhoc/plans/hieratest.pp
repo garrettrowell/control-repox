4072d32cd967233eca41721511b26482bd2beeff
Garrett Rowell <garrett@puppet.com>
2023-07-12 12:46:02 -0400

    askdjlf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -4,13 +4,14 @@ plan adhoc::hieratest(
   TargetSpec $targets,
 ) {
 
-  $test_secret = apply(get_target('pe-primary.garrett.rowell')) {
-    notify { 'test-secret':
-      message => "${lookup('test-secret').unwrap}"
+  $lookup_secret = 'test-secret'
+  $secret_block = apply(get_target('pe-primary.garrett.rowell')) {
+    notify { $lookup_secret:
+      message => "${lookup($lookup_secret).unwrap}"
     }
   }
-  $retrieved_test_secret = $test_secret.to_data[0]['value']['report']['resource_statuses'].keys[0]#['events']['desired_value']
-  out::message($retrieved_test_secret)
+  $retrieved_secret = $secret_block.to_data[0]['value']['report']['resource_statuses']["Notify[${lookup_secret}]"]['events']['desired_value']
+  out::message($retrieved_secret)
   #$test = lookup('test_secret')
   #out::message($test)
   #$test_pass = lookup('atest')
