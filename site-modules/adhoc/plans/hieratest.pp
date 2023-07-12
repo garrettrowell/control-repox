31ed24af3f6adca87b808df49a8c7f5abfbd4299
Garrett Rowell <garrett@puppet.com>
2023-07-12 12:57:37 -0400

    asdf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -2,20 +2,19 @@
 plan adhoc::hieratest(
   # input parameters go here
   TargetSpec $targets,
+  String $lookup_secret,
 ) {
 
-  $lookup_secret = 'test-secret'
+  # lookup the desired secret on the primary server because we assume the actual target(s) won't have a puppet agent
   $secret_block = apply(get_target('pe-primary.garrett.rowell')) {
     notify { $lookup_secret:
       message => "${lookup($lookup_secret).unwrap}"
     }
   }
+  # take advantage of only ONE result block with that always creates ONE corrective change
   $retrieved_secret = $secret_block.to_data[0]['value']['report']['resource_statuses']["Notify[${lookup_secret}]"]['events'][0]['desired_value']
-  out::message($retrieved_secret)
-  #$test = lookup('test_secret')
-  #out::message($test)
-  #$test_pass = lookup('atest')
 
-  # out::message($test_pass)
+  #purely for development...
+  out::message($retrieved_secret)
 
 }
