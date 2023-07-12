b16ac5edcc28e3ef7e030dbbbfbd8eaf0e2b8a2d
Garrett Rowell <garrett@puppet.com>
2023-07-12 12:08:45 -0400

    asdf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -5,7 +5,7 @@ plan adhoc::hieratest(
 ) {
 
   $test_pass = apply(get_target('pe-primary.garrett.rowell')) {
-    lookup('test_secret').unwrap
+    lookup('test-secret').unwrap
     #echo { 'test_password':
     #  message => lookup('test_password').unwrap
     #}
