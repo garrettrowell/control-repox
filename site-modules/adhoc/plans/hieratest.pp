705707d611c741433acc51177d84083b1ad464f7
Garrett Rowell <garrett@puppet.com>
2023-07-13 12:09:20 -0400

    asdf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -21,14 +21,14 @@ plan adhoc::hieratest(
 
     case $type {
       'secret': {
-        parallelize($targets) |$target| {
+        parallelize(get_targets($targets)) |$target| {
         #get_targets($targets).each |$target| {
           run_command("printf '${retrieved_secret}'", $target, "printf ${lookup_secret} on ${target}")
         }
       }
       'certificate': {
         $certfile = '/tmp/test_cert.pkcs12'
-        parallelize($targets) |$t| {
+        parallelize(get_targets($targets)) |$t| {
         #get_targets($targets).each |$target| {
           run_command("base64 --decode <<< '${retrieved_secret}' > ${certfile}", $target, "write certificate to ${certfile} on ${target}")
           run_command("openssl pkcs12 -in ${certfile} -info -nokeys -passout pass: -passin pass:''", $target, "reading ${certfile} on ${target}")
