4f3ffac370c879610fbc138ad979e620744d0f66
Garrett Rowell <garrett@puppet.com>
2023-07-13 11:56:02 -0400

    sdaf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -34,11 +34,13 @@ plan adhoc::hieratest(
       }
     }
   } else {
-    $err = $secret_block.to_data[0]['_error']
-    out::message({
-      'kind'    => $err['kind'],
-      'message' => $err['msg']
-    })
+    $err = $secret_block.to_data[0]
+    out::message($err)
+
+    #    out::message({
+    #      'kind'    => $err['kind'],
+    #      'message' => $err['msg']
+    #    })
   }
 
 }
