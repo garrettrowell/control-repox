864d205358fb8b7da75dd3709be3576eb65d94bf
Garrett Rowell <garrett@puppet.com>
2023-07-13 11:48:16 -0400

    asdf

--- site-modules/adhoc/plans/hieratest.pp
+++ site-modules/adhoc/plans/hieratest.pp
@@ -37,7 +37,7 @@ plan adhoc::hieratest(
     #$err = $secret_block['_error']
     #out::message("${err['kind']}, ${err['msg']}")
     $err = $secret_block[0].error
-    $err
+    log::error($err)
     #out::message("${err}")
   }
 
