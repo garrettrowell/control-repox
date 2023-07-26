c585538bdf5aaac6a767454e64e38a5ad143da14
Garrett Rowell <garrett@puppet.com>
2023-07-26 10:58:07 -0400

    asd

--- site-modules/adhoc/plans/azure_test.pp
+++ site-modules/adhoc/plans/azure_test.pp
@@ -2,8 +2,12 @@
 plan adhoc::azure_test(
   # input parameters go here
   TargetSpec $targets,
+  String $secret,
+  String $pkcs12_location,
 ) {
 
-  # plan steps go here
+  $targets = get_targets($target)
+  $cert_from_azure = lookup('secret')
+  run_command("openssl enc -base64 -d -A <<< '${cert_from_azure}' > ${pkcs12_location}", $targets)
 
 }
