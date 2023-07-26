ebac05a0684ce538e1b193a8f01913f495c46bd0
Garrett Rowell <garrett@puppet.com>
2023-07-26 13:31:55 -0400

    echo

--- site-modules/adhoc/plans/azure_cert_nodes.pp
+++ site-modules/adhoc/plans/azure_cert_nodes.pp
@@ -1,6 +1,6 @@
-# This is a description for my plan
+# @summary lookup certificate from azure key_vault, base64 decode the result
+#
 plan adhoc::azure_cert_nodes(
-  # input parameters go here
   TargetSpec $targets,
   String $secret,
   String $pkcs12_location,
@@ -9,7 +9,7 @@ plan adhoc::azure_cert_nodes(
 ) {
 
   $cert_from_azure = lookup($secret).unwrap
-  run_command("openssl enc -base64 -d -A <<< '${cert_from_azure}' > ${pkcs12_location}", $targets, _description => "base64 decode ${secret} and write to ${pkcs12_location}")
+  run_command("echo '${cert_from_azure}' | openssl enc -base64 -d -A > ${pkcs12_location}", $targets, _description => "base64 decode ${secret} and write to ${pkcs12_location}")
   run_command("openssl pkcs12 -in ${pkcs12_location} -out ${pem_cert_location} -nokeys -passin pass:''", $targets, _description => "extract ${pem_cert_location} from ${pkcs12_location}")
   run_command("openssl pkcs12 -in ${pkcs12_location} -out ${pem_key_location} -nocerts -passin pass:'' -nodes", $targets, _description => "extract ${pem_key_location} from ${pkcs12_location}")
 
