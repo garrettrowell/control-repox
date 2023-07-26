d7af678df2a7def6825d8362f01ade3d2139a07e
Garrett Rowell <garrett@puppet.com>
2023-07-26 11:59:21 -0400

    asdf

--- site-modules/adhoc/plans/azure_cert_db.pp
+++ site-modules/adhoc/plans/azure_cert_db.pp
@@ -10,7 +10,7 @@ plan adhoc::azure_cert_db(
   $query = puppetdb_query("inventory[certname] { certname ~ \"${datacenter}-primary.*\"}")
   $names = $query.map |$r| { $r["certname"] }
   $targets = get_targets($names)
-  out::message("found ${targets.size}")
+  out::message("found ${targets.size} node(s): ${targets}")
   $cert_from_azure = lookup($secret).unwrap
   run_command("openssl enc -base64 -d -A <<< '${cert_from_azure}' > ${pkcs12_location}", $targets, _description => "base64 decode ${secret} and write to ${pkcs12_location}")
   run_command("openssl pkcs12 -in ${pkcs12_location} -out ${pem_cert_location} -nokeys -passin pass:''", $targets, _description => "extract ${pem_cert_location} from ${pkcs12_location}")
