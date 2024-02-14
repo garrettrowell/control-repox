1abc2ae1200b877d937498d191a425045253ee02
Garrett Rowell <garrett@puppet.com>
2024-02-14 15:21:21 -0800

    silly

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,6 +26,8 @@ File { backup => false }
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
   echo { 'im new': }
+  echo { 'im newer': }
+
   #  ini_setting {
   #    default:
   #      path    => '/etc/puppetlabs/puppet/puppet.conf',
